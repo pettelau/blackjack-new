5a4b2989e58140a75a7b274ac5ac211c3c023032
Petter Lauvrak <petter.lauvrak@hotmail.com>
2020-02-24 15:50:21 +0100

    alle kommentarer lagt inn

--- index ferdig.js
+++ index ferdig.js
@@ -92,7 +92,7 @@ let dealerKort4;
 let dealerKort5;
 let dealerKort6;
 let dealerKort7;
-let dealerKort8; //alle dealerkort
+let dealerKort8; //alle dealerkort. disse blir senere tildelt en verdi fra 1-13
 
 let spillerKort1;
 let spillerKort2;
@@ -101,13 +101,14 @@ let spillerKort4;
 let spillerKort5;
 let spillerKort6;
 let spillerKort7;
-let spillerKort8;
+let spillerKort8; //alle spillerkort. disse blir senere tildelt en verdi fra 1-13
 
-let trekkNyKnapp;
+let trekkNyKnapp; //lager noen variabler for knapper som blir brukt senere
 let staKnapp;
 let nyttSpillKnapp;
 
-let nyttSpillCounter = 0;
+let nyttSpillCounter = 0; //null runder spilt. Dette blir brukt senere for å opprette nye knapper kun den første runden det blir spilt, slik at ikke nye ting
+//oppretter hver gang
 let dealerKortArray = [];
 let innsats = document.getElementById("innsats");
 let bilde1 = document.getElementById("bilde1");
@@ -135,21 +136,24 @@ let spillerSum = 0;
 let runderSpilt = 0;
 //visSaldo();
 function visSaldo() {
+  //en funksjon som blir kallet på hver gang det skjer oppdateringer i brukerens saldo.
   document.getElementById("saldo").innerText = "Saldo: " + parseInt(saldo);
   connection.query("UPDATE Leaderboard SET score=? WHERE username=?", [
     (parseInt(saldo) / 1000) * 100,
     emailLoggInn.value
-  ]);
-  leaderboardfunc();
-  counter = 0;
-  leaderboardList.innerHTML = "";
+  ]); //oppdateer også saldo i databasen
+  leaderboardfunc(); //kaller på leaderboard funksjonen slik at leaderboard blir oppdatert kontinuerlig
+  counter = 0; //reseter counter som hører til leaderboard, slik at for-løkken som printer ut de fem beste brukerne kjører på nytt
+  leaderboardList.innerHTML = ""; //sletter gammel leaderboard slik at kun den oppdaterte blir vist.
 }
 
 sjekkInnsatsKnapp.onclick = () => {
+  //sjekker at innsatsen er mindre eller lik saldo, hvis ikke kan ikke brukeren spille.
   if (innsats.value > saldo || saldo < 0) {
     alert("Innsatsen overskrider saldoen din");
     document.getElementById("oppstart").style.visibility = "hidden";
   } else {
+    //synliggjør diverse knapper når saldo er godkjent og bruker er klar til å spille
     document.getElementById("oppstart").style.visibility = "visible";
     document.getElementById("sjekkInnsats").disabled = true;
     document.getElementById("innsats").disabled = true;
@@ -157,10 +161,10 @@ sjekkInnsatsKnapp.onclick = () => {
 };
 var spillerKortArray;
 oppstartKnapp.onclick = () => {
-  saldo = saldo - innsats.value;
+  // dette er på en måte hovedfunksjonen som starter en runde med blackjack
+  saldo = saldo - innsats.value; //innsatsen trekkes fra saldo, og saldo oppdateres
   visSaldo();
-  console.log("saldo: " + saldo);
-  console.log(innsats.value);
+
   dealerKort1 = alleKort[Math.floor(Math.random() * 13)];
   dealerKort2 = alleKort[Math.floor(Math.random() * 13)];
   dealerKort3 = alleKort[Math.floor(Math.random() * 13)];
@@ -168,7 +172,7 @@ oppstartKnapp.onclick = () => {
   dealerKort5 = alleKort[Math.floor(Math.random() * 13)];
   dealerKort6 = alleKort[Math.floor(Math.random() * 13)];
   dealerKort7 = alleKort[Math.floor(Math.random() * 13)];
-  dealerKort8 = alleKort[Math.floor(Math.random() * 13)];
+  dealerKort8 = alleKort[Math.floor(Math.random() * 13)]; //alle dealerkort får en verdi
 
   spillerKort1 = alleKort[Math.floor(Math.random() * 13)];
   spillerKort2 = alleKort[Math.floor(Math.random() * 13)];
@@ -177,18 +181,19 @@ oppstartKnapp.onclick = () => {
   spillerKort5 = alleKort[Math.floor(Math.random() * 13)];
   spillerKort6 = alleKort[Math.floor(Math.random() * 13)];
   spillerKort7 = alleKort[Math.floor(Math.random() * 13)];
-  spillerKort8 = alleKort[Math.floor(Math.random() * 13)];
+  spillerKort8 = alleKort[Math.floor(Math.random() * 13)]; //alle dealerkort får en verdi
 
-  spillerKortArray = [spillerKort1, spillerKort2];
+  spillerKortArray = [spillerKort1, spillerKort2]; // de to første spillerkortene legges inn i et array
 
-  bilde1.src = alleKortBilder[dealerKort1 - 1];
+  bilde1.src = alleKortBilder[dealerKort1 - 1]; //dealerkort 1 vises på siden
   document.getElementById("currentPointDealer").innerHTML =
-    alleKortPoeng[dealerKort1 - 1];
+    alleKortPoeng[dealerKort1 - 1]; //dealerens poengsum vises
 
-  bilde9.src = alleKortBilder[spillerKort1 - 1];
+  bilde9.src = alleKortBilder[spillerKort1 - 1]; //spillerens to første kort vises
   bilde10.src = alleKortBilder[spillerKort2 - 1];
 
   if (runderSpilt == 0) {
+    //hvis dette er første runde vil det blir oppdatert knapper for å trekke nytt kort og å stå på nåværende poengsum
     trekkNyKnapp = document.createElement("button");
     trekkNyKnapp.innerHTML = "Trekk nytt kort";
     document.body.appendChild(trekkNyKnapp);
@@ -197,16 +202,17 @@ oppstartKnapp.onclick = () => {
     staKnapp.innerHTML = "Stå";
     document.body.appendChild(staKnapp);
 
-    runderSpilt++;
+    runderSpilt++; //runderspilt blir til 1 og dette skjer altså ikke på den eventuelt neste runden som blir spilt
   }
 
   staKnapp.style.visibility = "visible";
   trekkNyKnapp.style.visibility = "visible";
 
-  steg2();
-};
+  steg2(); // alt som skjer etter dette måtte settes inn i en veldig stor funksjon ( steg2() ), siden JS kjører gjennom hele koden når siden lastes ble det krøll
+  //lenger nede i koden. Derfor kalles ikke dette på før her.
+}; //ikke i bruk
 
-function currentValueold(trukkedeKort) {
+/* function currentValueold(trukkedeKort) {
   trukkedeKort.sort(aceToBack);
   console.log(trukkedeKort);
   let sum = 0;
@@ -220,8 +226,12 @@ function currentValueold(trukkedeKort) {
     }
   }
   return sum;
-}
+} */ //BJ er laget sånn at Essene kan ha båd verdi som 1 eller 11, ut ifra hva som er best for spilleren/dealeren. Hvis summen overskrider 21 når essen telles som 11,
+//vil essen istedenfor telles som 1. De to funksjonene under finner ut av hva som er mest gunstig for spilleren og gjør dette
+//Disse to funksjonene vil være lettere og gi mer mening dersom man ser på funksjonen steg2() under først.
+
 function aceToBack(a, b) {
+  //funksjon som setter alle essene bakerst i arrayet for spiller og dealer.
   if (a == 1 || b == 1) {
     if (b == 1) {
       return -1;
@@ -233,57 +243,71 @@ function aceToBack(a, b) {
 }
 
 function currentValue(trukkedeKort) {
+  //denne funksjonen gir spilleren/dealeren den høyeste mulige poengsummen med essene
   trukkedeKort.sort(aceToBack);
   let sum = 0;
   for (let i = 0; i < trukkedeKort.length; i++) {
     if (trukkedeKort[i] != 1) {
+      //hvis kortet ikke er et ess, blir det naturligvis bare lagt til i summen
       sum += parseInt(alleKortPoeng[trukkedeKort[i] - 1]);
     } else {
       if (typeof trukkedeKort[i + 1] != "undefined") {
-        sum += 1;
+        //hvis man har fått TO ELLER FLERE ess i samme runde, vil alle essene unntatt den siste
+        //telles som 1, fordi to ess som teller som 11 blir 22 og da har man uansett gått over 21, noe man ikke ønsker.
+        sum += 1; //så alle essene som ikke er det siste esset får summen 1
       } else {
         if (sum + 11 >= 22) {
+          //hvis det siste esset går over 21 dersom det teller som 11, blir det lagt til som 1
           sum += 1;
         } else {
+          //hvis ikke det går over 21, blir det lagt til som 11.
           sum += 11;
         }
       }
     }
   }
-  console.log(sum);
-  console.log(trukkedeKort);
-  return sum;
+  return sum; //summen, som altså er den mest gunstige summeren spilleren/dealeren kan ha ut ifra kortene, blir returnert av funksjonen
 }
 
 function steg2() {
-  let at = 0;
+  //her begynner "del 2" av spillet, hvor brukeren faktisk må begynne å ta valg (stå eller trekke nytt kort)
+  let at = 0; //denne variablen spier hvor mange ganger spilleren har trykket på "trekk nytt kort"-knappen, som er en egen funksjon under ("trekkNyKnapp.onclick")
   document.getElementById("currentPoint").innerText = currentValue(
     spillerKortArray
-  );
+  ); //nåværende poengsum vises på siden etter å ha kjørt gjennom currentValue-funksjonen, som alltid viser riktig sum
   if (currentValue(spillerKortArray) == 21) {
+    //hvis man får det spesielle tilfellet akkurat 21 poeng (Blackjack),
     setTimeout(function() {
       alert("Blackjack babyyyyy");
     }, 500);
     staFunksjon();
-    saldo += innsats.value * 0.5;
+    if (currentValue(dealerKortArray) !== 21) {
+      //hvis dealer ikke har blackjack samtidig har spilleren automatisk vunnet og innsats ganges med 0.5 ekstra (BJ-regel)
+      saldo += innsats.value * 0.5;
+    }
     visSaldo();
   }
 
   trekkNyKnapp.onclick = () => {
+    //spilleren velger å trekke nytt kort
     trekkFunksjon();
   };
   function trekkFunksjon() {
+    //da kalles denne funksjonen
     if (at == 0) {
-      bilde11.src = alleKortBilder[spillerKort3 - 1];
+      //at starter som 0 så denne if-setningen kjøres da uansett
+      bilde11.src = alleKortBilder[spillerKort3 - 1]; //kort 3 vises
       spillerKortArray.push(spillerKort3);
       document.getElementById("currentPoint").innerText = currentValue(
         spillerKortArray
       );
       if (currentValue(spillerKortArray) > 21) {
-        resultat(dealerKortArray);
+        resultat(dealerKortArray); //hvis spilleren nå har over 21 hoppes det rett på resultat, og man har tapt.
       }
     }
     if (at == 1) {
+      //at plusses på med 1 i bunnen av trekkFunksjon, så hvis spilleren velger å trekke enda et kort vil denne if-setningen kjøre
+      //herfra og ned skjer egentlig akkurat det samme hver gang: Er spilleren fortsatt under 21 kan han/hun trekke nytt kort, kommer spilleren over 21 kjøres resultat-funksjon.
       bilde12.src = alleKortBilder[spillerKort4 - 1];
       spillerKortArray.push(spillerKort4);
       document.getElementById("currentPoint").innerText = currentValue(
@@ -334,11 +358,9 @@ function steg2() {
         resultat(spillerKortArray);
       }
     }
-    at++;
-
-    console.log(spillerSum);
+    at++; // at = antall trekk. Viser hvor mange ganger spilleren har trykket på Trekk nytt kort.
   }
-
+  //staknapp() viser automatisk kort nr 2 for dealer, oppdaterer forløpig totalsum og kjører neste kortfunk etter 1.5 sek dersom totalsum er >17
   staKnapp.onclick = () => {
     staFunksjon();
   };
@@ -355,6 +377,7 @@ function steg2() {
       setTimeout(function() {
         kort3();
       }, 1500);
+      //her kommer funkesjoner for kort 3,4,5,6,7 og 8. Funkjsonen viser neste bildekort, pusher kortverdi inn i array og viser forløpig totalsum. Dersom totalsumen til dealer er under 17 kjøres neste kortfunksjon etter 1.5 sek for bedre visuell opplevelse. Man kan maksimalt trekke 8 kort i dette spilet, i teorien kan man trekke 21 ess på rad siden man vanligvis spiller med 8 kortstokker, men vi har begrenset antall kort til 8 her da det er svært usansynlig at man trekker mer en dette uansett.
       function kort3() {
         console.log(currentValue(dealerKortArray));
         bilde3.src = alleKortBilder[dealerKort3 - 1];
@@ -440,10 +463,13 @@ function steg2() {
       document.getElementById("currentPointDealer").innerText = currentValue(
         dealerKortArray
       );
+      resultat();
     }
-    console.log("Andre dealersum: " + dealerSum);
-    console.log("Andre spillersum: " + spillerSum);
+    //tester diverse outputs med console.log
+    // console.log("Andre dealersum: " + dealerSum);
+    // console.log("Andre spillersum: " + spillerSum);
   }
+  //resultat() sjekker resultatet, seier/tap/push og gir tilsvarende output. Oppdaterer også saldo til neste runde
   function resultat() {
     if (currentValue(dealerKortArray) > 21) {
       document.getElementById("ut1").innerHTML =
@@ -468,6 +494,7 @@ function steg2() {
         document.getElementById("ut1").innerHTML = "<br>" + resultatTap;
       }
     }
+    //visSaldo() lager en nyspillknapp første runde ved hjelp av counter, alle andre runder blir knappen kun gjort synlig/usynlig
     visSaldo();
     if (nyttSpillCounter == 0) {
       nyttSpillKnapp = document.createElement("button");
@@ -476,12 +503,13 @@ function steg2() {
       nyRunde();
       nyttSpillCounter += 1;
     }
+    //fjerner knappene som man spiller med mens man starter ny runde
     nyttSpillKnapp.style.visibility = "visible";
     staKnapp.style.visibility = "hidden";
     trekkNyKnapp.style.visibility = "hidden";
   }
 }
-//bhhbbuhb
+//nyRunde() gjør innsats inputt og knapp tilgjenglig til en nyrunde. Tømmer også all spilldata som spillerarrays, dealerarrays samt all spillutdata som bilder og tekst.
 function nyRunde() {
   nyttSpillKnapp.onclick = () => {
     document.getElementById("sjekkInnsats").disabled = false;
@@ -507,7 +535,7 @@ function nyRunde() {
     document.getElementById("ut1").innerHTML = "";
     document.getElementById("currentPoint").innerHTML = "";
     document.getElementById("currentPointDealer").innerHTML = "";
-
+    //fjerner knappen til neste spillrunde
     nyttSpillKnapp.style.visibility = "hidden";
   };
 }
